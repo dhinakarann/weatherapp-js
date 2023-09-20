a961b3d007c928c46d9edd3daec4238acce0e3b0
sai verma <vectorvarma0303@gmail.com>
2023-09-20 20:07:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
+const API_KEY = "your api key";
 
-const API_KEY = "998834fdc9415cbcf817433207a3f0ff"
+function renderWeatherData(data){
 
-async function showWeather() {
-    
-    let city = "goa";
+    let newPara = document.createElement("p");
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-   
-    const data = await response.json();
-   console.log("weather Data:->", data);
-   
-   let newPara = document.createElement('p');
-   newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
-
-   document.body.appendChild(newPara);
+    document.body.appendChild(newPara);
 }
-showWeather() 
+
+async function fetchWeatherDetails() {
+    try {
+        let city = "goa";
+
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+
+        const data = await response.json();
+        // console.log("weather Data:->", data);
+        renderWeatherData(data);
+
+        render
+    } catch (err) {
+        //error should be handled here
+    }
+}
+
+async function getCustomWeatherDetails(){
+   try{
+    let lat = 15.6333;
+    let lon = 18.3333;
+
+    let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    let data = await result.json()
+
+    console.log(data);
+   }
+   catch(err){
+    console.log("error found", err);
+   }
+}
+
+getCustomWeatherDetails()
+fetchWeatherDetails()
